df04cdb87aa37cf9efe78064dd197e7679931910
Riccardo Cossu <riccardo.cossu@gmail.com>
2014-10-20 15:57:01 +0200

    fixed a possible problem with input encoding in csv preparing release 0.2.1

--- i18split-base/src/test/groovy/net/riccardocossu/i18split/base/csv/CvsInputDriverTest.groovy
+++ i18split-base/src/test/groovy/net/riccardocossu/i18split/base/csv/CvsInputDriverTest.groovy
@@ -22,4 +22,32 @@ public class CvsInputDriverTest {
 
 	}
 
+	@Test
+	public void csvInputDriverShouldNotReadCorrectlyAFileWithEncodingISO8859IfConfiguredToReadUTF8() {
+		CsvInputDriver driver = new CsvInputDriver();
+		Configuration conf = new BaseConfiguration()
+		conf.addProperty(ConfigKeys.INPUT_BASE_PATH, "src/test/resources/engine/inCsvOutProperties")
+		conf.addProperty(CsvInputDriver.FILE_NAME, "encoding_UTF-8.csv")
+		conf.addProperty(ConfigKeys.INPUT_ENCODING, "ISO_8859-1")
+		String[] header = driver.init(conf)
+		assertEquals(1,header.length)
+		assertEquals("it",header[0])
+		def row = driver.readNext()
+		assertFalse(row.values["it"].equals("età"))
+	}
+
+	@Test
+	public void csvInputDriverShouldReadCorrectlyAFileWithEncodingISO8859IfConfiguredToReadIt() {
+		CsvInputDriver driver = new CsvInputDriver();
+		Configuration conf = new BaseConfiguration()
+		conf.addProperty(ConfigKeys.INPUT_BASE_PATH, "src/test/resources/engine/inCsvOutProperties")
+		conf.addProperty(CsvInputDriver.FILE_NAME, "encoding_ISO_8859-1.csv")
+		conf.addProperty(ConfigKeys.INPUT_ENCODING, "ISO_8859-1")
+		String[] header = driver.init(conf)
+		assertEquals(1,header.length)
+		assertEquals("it",header[0])
+		def row = driver.readNext()
+		assertEquals("età",row.values["it"])
+	}
+
 }
