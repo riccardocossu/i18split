2dc6091e851a38085c7c4a980dab2167d352d2dd
Riccardo Cossu <riccardo.cossu@gmail.com>
2015-05-28 12:03:56 +0200

    better error messages

--- i18split-base/src/main/groovy/net/riccardocossu/i18split/base/service/Engine.groovy
+++ i18split-base/src/main/groovy/net/riccardocossu/i18split/base/service/Engine.groovy
@@ -4,23 +4,28 @@ import net.riccardocossu.i18split.base.config.ConfigKeys
 import net.riccardocossu.i18split.base.driver.FilterDriver
 import net.riccardocossu.i18split.base.driver.InputDriver
 import net.riccardocossu.i18split.base.driver.OutputDriver
+import net.riccardocossu.i18split.base.model.DataRow;
+
 import org.apache.commons.configuration.Configuration
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Engine {
+	private static final Logger log = LoggerFactory.getLogger(Engine.class)
 
 	private static Map<String, FilterDriver> inputPluginsByShortName = [:]
 
 	static {
 		ServiceLoader<FilterDriver> loader = ServiceLoader
-				.load(FilterDriver.class);
-		Iterator<FilterDriver> it = loader.iterator();
+				.load(FilterDriver.class)
+		Iterator<FilterDriver> it = loader.iterator()
 		while (it.hasNext()) {
-			FilterDriver driver = it.next();
-			String shortName = driver.getShortName();
+			FilterDriver driver = it.next()
+			String shortName = driver.shortName
 			// if driver doesn't want or need to be called by shortname, it
 			// should set this method to return null
 			if (shortName != null) {
-				inputPluginsByShortName.put(shortName, driver);
+				inputPluginsByShortName.put shortName, driver
 			}
 		}
 	}
@@ -29,26 +34,27 @@ public class Engine {
 
 
 	public Engine(Configuration configuration) {
-		super();
+		super()
 		this.configuration = configuration
 	}
 
 	public void process() {
 		def inputDriver = configuration.getString(ConfigKeys.INPUT_DRIVER)
-        // FIXME deal with classcast and NPE
-        InputDriver reader = inputPluginsByShortName.get(inputDriver)
+		InputDriver reader = null
+		OutputDriver writer = null
         def outputDriver = configuration.getString(ConfigKeys.OUTPUT_DRIVER)
-        OutputDriver writer = inputPluginsByShortName.get(outputDriver)
 		boolean keepOrder = configuration.getBoolean(ConfigKeys.KEEP_ORDER,false)
         try {
-            def usedKeys = reader.init(configuration)
+			reader = (InputDriver)inputPluginsByShortName[inputDriver]
+			writer = (OutputDriver)inputPluginsByShortName[outputDriver]
+            def usedKeys = reader.init configuration
             configuration.addProperty(ConfigKeys.INPUT_KEYS, usedKeys)
-            writer.init(configuration)
+            writer.init configuration
             def rows = []
-            def row = reader.readNext()
+            DataRow row = reader.readNext()
             while (row != null) {
 				if(row.values) {
-					rows.add(row)
+					rows.add row
 				}
                 row = reader.readNext()
             }
@@ -57,11 +63,17 @@ public class Engine {
 				rows = rows.sort {it.key}
 			}
             for (r in rows) {
-				writer.writeRow(r)
+				writer.writeRow r
             }
-        } finally {
-            reader.close()
-            writer.close()
+        } catch(Exception e) {
+			log.error("Conversion error", e)
+        }  finally {
+			if(reader) {
+				reader.close()
+			}
+			if(writer) {
+				writer.close()
+			}
         }
 	}
 
